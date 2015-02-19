f7cc5a745fed5ed09378a9809c601910a5d0a3ce
jigartailor1984 <jigartailor1984@live.com>
2015-02-19 18:19:38 +0530

    Implemented Common View model base

--- MVVMFramework/ViewModelBase.cs
+++ MVVMFramework/ViewModelBase.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVMFramework
 {
-    public class ViewModelBase
+    public class ViewModelBase : INotifyPropertyChanged
     {
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value))
+                return false;
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            var eventHandler = this.PropertyChanged; 
+            if (eventHandler != null) 
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
