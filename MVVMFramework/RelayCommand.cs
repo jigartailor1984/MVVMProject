f71837b36201b44bc0877c2c6407cdd08ad1212c
jigartailor1984 <jigartailor1984@live.com>
2015-02-19 18:44:28 +0530

    Implemented Relay Command

--- MVVMFramework/RelayCommand.cs
+++ MVVMFramework/RelayCommand.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MVVMFramework
 {
-    public class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private Action<object> _action;
+
+        private Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> action)
+        {
+            _action = action;
+            _canExecute = new Predicate<object>((parameter) => { return true; });
+        }
+
+        public RelayCommand(Action<object> action, Predicate<object> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
     }
 }
